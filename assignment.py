f8d9abbb6a4aa9da7a2e5e19f616586dec26c0ea
Samareen Sharook Hussain <s.sharookhussain@students.uu.nl>
2026-01-12 17:42:46 +0100

    filtred for seattle

--- assignment.py
+++ assignment.py
@@ -5,3 +5,13 @@ with open ('precipitation.json', encoding = 'utf-8') as file:
 print(data)
 print(type(data))
 
+# 0.3: filtering for seattle
+
+seattle_data = []
+for element in data:
+    if element['station'] == 'GHCND:US1WAKG0038':
+        seattle_data.append(element)
+print(seattle_data)
+
+
+
