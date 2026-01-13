3664b2e5005d463f66eaf0a9cb6d775ba89ff675
Samareen Sharook Hussain <s.sharookhussain@students.uu.nl>
2026-01-13 14:34:51 +0100

    dictionary done

--- assignmentpart2.py
+++ assignmentpart2.py
@@ -8,16 +8,28 @@ with open ('precipitation.json', encoding = 'utf-8') as file:
 
 print(data)
 print(type(data))
-stations = []
+results_dic ={}
 with open('stations.csv', mode = 'r') as csvfile:
+    per_line = csvfile.readline()
     #information = csv.DictReader(csvfile)
     for lines in csvfile:
         line_strip = lines.strip()
         lines_split = line_strip.split(",")
         print(lines_split)
         
+        results_dic[lines_split[0]] = {
+            'state': lines_split[1],
+            'station': lines_split[2]
+            
+        }
+print(results_dic)
 
-results = {}
+
+
+    
+
+
+
 
 # stations = []
 # stations = set() ### distinct operation
